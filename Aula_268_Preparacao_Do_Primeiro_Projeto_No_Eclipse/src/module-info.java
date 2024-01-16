aa9b373a61139d96d7c7d1fa2fb6b8b7a9f1a981
Rafael Cecyn <rafael.mendes112@hotmail.com>
2024-01-16 19:11:59 -0300

    Secao sobre JDBC

---
+++ Aula_268_Preparacao_Do_Primeiro_Projeto_No_Eclipse/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Aula_268_Preparacao_Do_Primeiro_Projeto_No_Eclipse {
+	requires java.sql;
+}
---
+++ Aula_271_Demo_Inserir_Dados/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Aula_270_Demo_Recuperar_Dados {
+	requires java.sql;
+}
